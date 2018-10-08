f20ab82be0f99eced0361bfef0baddc2bd0763bb
chitraphp <chitra.atmakuru@gmail.com>
2018-10-07 18:15:18 -0700

    add all

---
+++ Assignment_6/LongestCommonPrefix.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment_6
+{
+    class TrieNode
+    {
+
+        // R links to node children
+        private TrieNode[] links;
+
+        private const int R = 26;
+
+        private bool isEnd;
+        private int Size = 0;
+
+        public TrieNode()
+        {
+            links = new TrieNode[R];
+        }
+
+        public bool containsKey(char ch)
+        {
+            return links[ch - 'a'] != null;
+        }
+        public TrieNode get(char ch)
+        {
+            return links[ch - 'a'];
+        }
+        
+        public void setEnd()
+        {
+            isEnd = true;
+        }
+        public bool IsEnd()
+        {
+            return isEnd;
+        }
+        public void Put(char ch, TrieNode node)
+        {
+            links[ch - 'a'] = node;
+            Size++;
+        }
+
+        public int getLinks()
+        {
+            return Size;
+        }
+    }
+
+    public class Trie
+    {
+        private TrieNode root;
+
+        public Trie()
+        {
+            root = new TrieNode();
+        }
+
+        // Inserts a word into the trie.
+        public void Insert(string word)
+        {
+            TrieNode node = root;
+            for (int i = 0; i < word.Length; i++)
+            {
+                char currentChar = word[i];
+                if (!node.containsKey(currentChar))
+                {
+                    node.Put(currentChar, new TrieNode());
+                    var y = node.getLinks();
+                }
+                node = node.get(currentChar);
+            }
+            node.setEnd();
+        }
+
+
+        //assume methods insert, search, searchPrefix are implemented as it is described
+        //in  https://leetcode.com/articles/implement-trie-prefix-tree/)
+        public string SearchLongestPrefix(string word)
+        {
+            TrieNode node = root;
+            StringBuilder prefix = new StringBuilder();
+            for (int i = 0; i < word.Length; i++)
+            {
+                char curLetter = word[i];
+                int x = node.getLinks();
+                if (node.containsKey(curLetter) && (node.getLinks() == 1) && (!node.IsEnd()))
+                {
+                    prefix.Append(curLetter);
+                    node = node.get(curLetter);
+                }
+                else
+                    return prefix.ToString();
+
+            }
+            return prefix.ToString();
+        }
+
+        
+    }
+    class LongestCommonPrefix
+    {
+       public string longestCommonPrefix(string q, string[] strs)
+        {
+            if (strs == null || strs.Length == 0)
+                return "";
+            if (strs.Length == 1)
+                return strs[0];
+            Trie trie = new Trie();
+            for (int i = 0; i < strs.Length; i++)
+            {
+                trie.Insert(strs[i]);
+            }
+            return trie.SearchLongestPrefix(q);
+        }
+        public void find()
+        {
+            Console.WriteLine("testing");
+        }
+    }
+}
--- Assignment_6/Program.cs
+++ Assignment_6/Program.cs
@@ -4,9 +4,79 @@ namespace Assignment_6
 {
     class Program
     {
+        public static int Value(char letter)
+        {
+            switch (letter)
+            {
+                case 'I':
+                    return 1;                    
+                case 'V':
+                    return 5;
+                case 'X':
+                    return 10;
+                case 'L':
+                    return 50;
+                case 'C':
+                    return 100;
+                case 'D':
+                    return 500;
+                case 'M':
+                    return 1000;
+                default:
+                    return -1;
+            }
+        }
+        public static int RomanToNumber(string str)
+        {
+            int res = 0;
+
+            // Traverse given input
+            for (int i = 0; i < str.Length; i++)
+            {
+                // Getting value of symbol s[i]
+                int s1 = Value(str[i]);
+
+                if (i + 1 < str.Length)
+                {
+                    // Getting value of symbol s[i+1]
+                    int s2 = Value(str[i + 1]);
+
+                    // Comparing both values
+                    if (s1 >= s2)
+                    {
+                        // Value of current symbol is greater
+                        // or equal to the next symbol
+                        res = res + s1;
+                    }
+                    else
+                    {
+                        res = res + s2 - s1;
+                        i++; // Value of current symbol is
+                             // less than the next symbol
+                    }
+                }
+                else
+                {
+                    res = res + s1;
+                    i++;
+                }
+            }
+            return res;
+
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            string str = "MCMIV";
+            //int output = RomanToNumber(str);
+            //Console.WriteLine(output);
+            //Console.ReadLine();
+            string s1 = "apple";
+            string[] strsArray = { "april", "ape" };            
+            LongestCommonPrefix lcs = new LongestCommonPrefix();
+            string prefix = lcs.longestCommonPrefix(s1, strsArray);
+            Console.WriteLine(prefix);
+            Console.ReadLine();
         }
     }
 }
